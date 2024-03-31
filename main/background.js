8db74f0114bce26bbfa344465ffc8439751d333e
Fuma820 <fuuma820@gmail.com>
2024-03-31 11:21:43 +0900

    タブの開く位置を変更する機能追加

--- main/background.js
+++ main/background.js
@@ -1,32 +1,272 @@
 //拡張機能インストール時実行
 chrome.runtime.onInstalled.addListener(() => {
     chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-        //開けるタブの最大値の初期値を現在のタブ数にする
+        //ストレージの初期値を設定
+        const bArray = ["0", "1", "0", "2", "0", "0", "0"];
+        const cArray = [false, false, true];
+        const rArray = ["50"];
+        const oArray = ["#ffcccc"];
+        const nArray = [50];
+        chrome.storage.local.set({ bArray: bArray, cArray: cArray, rArray: rArray, oArray: oArray, nArray: nArray });
         chrome.storage.local.set({ maxTabNum: tabs.length });
-        //アイコンの下に現在のタブ数を表示
-        chrome.action.setBadgeText({ text: String(tabs.length) });
+        chrome.storage.local.set({ check: false });
+        chrome.storage.local.set({ group: false });
+        chrome.storage.local.set({ currentTab: null });
+        //ストレージにタブの情報をグループごとに格納
+        makeGroups(tabs);
+        //アイコンの表示
+        displayNum(tabs.length, tabs.length, false);
+        makeIcon(tabs.length, tabs.length, false);
     });
 });
 
 //タブ更新時実行
-chrome.tabs.onUpdated.addListener((tabId) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-        //アイコンの下に現在のタブ数を表示
-        chrome.action.setBadgeText({ text: String(tabs.length) });
-        //ストレージに格納されているmaxTabNumよりタブ数が多ければ新しいタブを閉じる
-        chrome.storage.local.get("maxTabNum", (items) => {
-            if (tabs.length > items.maxTabNum) {
-                chrome.tabs.remove(tabId);
+        chrome.storage.local.get(["group", "maxTabNum", "check", "bArray", "cArray"], (items) => {
+            if (changeInfo.status === "complete") {//ページ読み込みが完了した時
+                makeGroups(tabs);
+                displayNum(tabs.length, items.maxTabNum, items.check, items.bArray[4]);
+                makeIcon(tabs.length, items.maxTabNum, items.check);
+                if (items.group == true) {
+                    tabGroup();
+                };
+            };
+        });
+    });
+});
+
+// タブが切り替わる時実行
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+    chrome.storage.local.get(["currentTab"], (items) => {
+        chrome.storage.local.set({ currentTab: activeInfo });
+    });
+});
+
+//タブ作成時実行
+chrome.tabs.onCreated.addListener((tab) => {
+    chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
+        chrome.storage.local.get(["maxTabNum", "check", "currentTab", "bArray", "cArray"], (items) => {
+            // 新しいタブを現在のタブの右に移動
+            if (items.bArray[6] != "0") {
+                var currentTabIndex = 0;
+                for (let element of tabs) {
+                    if (element.id == items.currentTab.tabId) {
+                        currentTabIndex = element.index;
+                        break;
+                    }
+                }
+                switch(items.bArray[6]){
+                    case "1":
+                        chrome.tabs.move(tab.id, { index: tabs.length });
+                        break;
+                    case "2":
+                        chrome.tabs.move(tab.id, { index: 0 });
+                        break;
+                    case "3":
+                        chrome.tabs.move(tab.id, { index: currentTabIndex + 1 });
+                        break;
+                    case "4":
+                        chrome.tabs.move(tab.id, { index: currentTabIndex });
+                        break;
+                }
             }
+            if (items.check == true && tabs.length > items.maxTabNum) {
+                chrome.tabs.remove(Number(tab.id));
+                if (items.cArray[2]) {
+                    makeNotify("タブ制限超過", items.maxTabNum + "個以上は開けません。")
+                };
+            }
+            displayNum(tabs.length, items.maxTabNum, items.check, items.bArray[4]);
+            makeIcon(tabs.length, items.maxTabNum, items.check);
+            duplicateVerify(tabs, tab, items.cArray, items.bArray[1]);
         });
     });
 });
 
 //タブ削除時実行
 chrome.tabs.onRemoved.addListener(() => {
-    //アイコンの下に現在のタブ数を表示
     chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-        chrome.action.setBadgeText({ text: String(tabs.length) });
+        chrome.storage.local.get(["maxTabNum", "check", "bArray"], (items) => {
+            makeGroups(tabs);
+            displayNum(tabs.length, items.maxTabNum, items.check, items.bArray[4]);
+            makeIcon(tabs.length, items.maxTabNum, items.check);
+            if (items.check == false) {
+                chrome.storage.local.set({ maxTabNum: tabs.length });
+            }
+        });
     });
 });
 
+//メッセージ取得時実行
+chrome.runtime.onMessage.addListener((data) => {
+    chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
+        chrome.storage.local.get(["maxTabNum", "check", "bArray"], (items) => {
+            switch (data) {
+                case "changeIcon":
+                    displayNum(tabs.length, items.maxTabNum, items.check, items.bArray[4]);
+                    makeIcon(tabs.length, items.maxTabNum, items.check);
+                    break;
+                case "group":
+                    tabGroup();
+                    break;
+                case "ungroup":
+                    tabUngroup();
+                    break;
+                case "checkON":
+                    displayNum(tabs.length, items.maxTabNum, items.check, items.bArray[4]);
+                    makeIcon(tabs.length, items.maxTabNum, items.check);
+                    break;
+                case "checkOFF":
+                    displayNum(tabs.length, items.maxTabNum, items.check, items.bArray[4]);
+                    makeIcon(tabs.length, items.maxTabNum, items.check);
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
+});
+
+//アイコンを作成する関数
+function makeIcon(tabsLength, maxTabNum, check) {
+    const canvas = new OffscreenCanvas(16, 16);
+    const context = canvas.getContext('2d');
+    const colorDegree = 255 - Math.pow(1 / 255, 1.5) * Math.pow(255 * (tabsLength / maxTabNum), 2.5) //限界値に近づくほど色合いの変化を上げる
+    context.clearRect(0, 0, 16, 16);
+    if (check == true) {
+        context.fillStyle = `rgb(255, ${colorDegree}, ${colorDegree})`; //白→赤のグラデーション
+    } else {
+        context.fillStyle = "rgb(0, 255, 0)"; //∞なら緑
+    }
+    context.fillRect(0, 0, 16, 16);
+    const imageData = context.getImageData(0, 0, 16, 16);
+    chrome.action.setIcon({ imageData: imageData }, () => {
+    });
+}
+//アイコン下に現在のタブ数を表示する関数
+function displayNum(tabsLength, maxTabNum, check, value = "0") {
+    switch (value) {
+        case "0": //通常表示
+            if (tabsLength == maxTabNum && check == true) {
+                chrome.action.setBadgeText({ text: String("MAX") });
+            } else if (check == true && maxTabNum < 100) {
+                chrome.action.setBadgeText({ text: String(tabsLength + "/" + maxTabNum) });
+            } else { //check == false
+                chrome.action.setBadgeText({ text: String(tabsLength) });
+            };
+            break;
+        case "1": //残数表示
+            if (check == true && maxTabNum - tabsLength < 10000) {
+                chrome.action.setBadgeText({ text: String(maxTabNum - tabsLength) });
+            } else {
+                chrome.action.setBadgeText({ text: String("∞") });
+            };
+            break;
+    };
+};
+//タブをグループ化する関数
+function tabGroup() {
+    chrome.storage.local.get(["tabGroups", "group", "bArray"], (items) => {
+        for (let i = 0; i < items.tabGroups.length; i++) {
+            //配列にタブのidを格納
+            let tabIdList = [];
+            for (let j = 3; j < items.tabGroups[i].length; j += 3) {
+                tabIdList.push(items.tabGroups[i][j]);
+            }
+            //グループ化
+            chrome.tabs.ungroup(tabIdList);
+            chrome.tabs.group({ tabIds: tabIdList });
+            chrome.storage.local.set({ group: true });
+            if (items.bArray[5] == "1") {
+                chrome.tabs.ungroup(tabIdList);
+            }
+        }
+    });
+}
+//グループを解除する関数
+function tabUngroup() {
+    chrome.storage.local.get(["tabGroups", "group"], (items) => {
+        for (let i = 0; i < items.tabGroups.length; i++) {
+            //配列にタブのidを格納
+            let tabIdList = [];
+            for (let j = 3; j < items.tabGroups[i].length; j += 3) {
+                tabIdList.push(items.tabGroups[i][j]);
+            }
+            //グループ解除
+            chrome.tabs.ungroup(tabIdList);
+            chrome.storage.local.set({ group: false });
+        }
+    });
+}
+//ストレージにタブの情報をグループごとに格納する関数
+function makeGroups(tabs) {
+    let tabGroups = [];
+    tabs.forEach((tab) => {
+        //ロード中のタブのURLを取得
+        if (tab.url == "") {
+            tab.url = tab.pendingUrl;
+        }
+        const info = tab.url.split("/");
+        let sameDomainNum = 0;
+        //index0にドメイン，1からはURL，タイトル，IDの順で格納
+        for (let i = 0; i < tabGroups.length; i++) {
+            const infoOfgroupI = tabGroups[i][1].split("/");
+            if (info[0] + info[2] == infoOfgroupI[0] + infoOfgroupI[2]) {
+                tabGroups[i].push(tab.url, tab.title, tab.id);
+                sameDomainNum++;
+            };
+        };
+        if (sameDomainNum == 0 && info[2] == "") {
+            const domain = info[3];
+            tabGroups.push([domain, tab.url, tab.title, tab.id]);
+        }
+        else if (sameDomainNum == 0) {
+            const domain = info[2];
+            tabGroups.push([domain, tab.url, tab.title, tab.id]);
+        };
+        chrome.storage.local.set({ tabGroups: tabGroups });
+    });
+};
+
+function duplicateVerify(tabs, target, cArray) {
+    let tabsArray = [];
+    if (tabs.length == 1) {
+        return -1;
+    }
+    if (cArray[1] == true || cArray[0] == true) {
+        let sameIndex = detectDuplicate(tabs, target)
+        if (sameIndex != -1) {
+            if (cArray[1] == true) {
+                if (cArray[0] == true) {
+                    makeNotify("重複タブ削除", tabs[sameIndex].title + "を削除しました。")
+                };
+                if (tabs[sameIndex].active) {
+                    chrome.tabs.remove(target.id);
+                } else {
+                    chrome.tabs.remove(tabs[sameIndex].id);
+                };
+            } else if (cArray[0] == true) {
+                makeNotify("重複タブ検知", tabs[sameIndex].title + "が重複しています。")
+            };
+        };
+    };
+
+    //urlのみでしか検索かけれん。
+    function detectDuplicate(tabs, target) {
+        tabs.forEach(function (value) {
+            tabsArray.push(value.url);
+        });
+        return tabsArray.indexOf(target.pendingUrl);
+    };
+}
+
+function makeNotify(title = "title未設定", message = "message未設定") {
+    const notify = {
+        type: "basic",
+        title: title,
+        message: message,
+        iconUrl: "./taber128.png"
+    };
+    chrome.notifications.create(notify);
+}
--- main/popup.js
+++ main/popup.js
@@ -1,39 +1,328 @@
-//main
 chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-    //タブ数を表示する
-    document.querySelector("#numOfTabs").innerHTML = tabs.length;
-    //全タブのタイトル，URLを表示する
-    let txt = "";
-    tabs.forEach((tab) => {
-        txt += `[${tab.title}] (${tab.url})\n\n`;
-    });
-    document.querySelector("#txt").value = txt;
-    //開けるタブの最大値を表示
-    chrome.storage.local.get("maxTabNum", (items) => {
-        document.querySelector("#maxTabNum").value = items.maxTabNum;
-    });
-    //設定できる値の最小値を現在のタブ数にする
-    document.querySelector("#maxTabNum").min = tabs.length;
-});
+    //popupの大きさ
+    if (screen.width <= 600) {//スクリーンが600px以下の場合
+        $("body").css("width", "300px");
+        $(".URLList").css("font-size", "10px");
+        $("#textarea").css("width", "270px");
+    }
+    else {//スクリーンが600pxより大きい場合
+        $("body").css("width", "400px");
+        $(".URLList").css("font-size", "16px");
+        $("#textarea").css("width", "370px");
+    }
 
-//event
-window.addEventListener("load", () => {
-    //copyボタンが押されたらtextareaの内容をクリップボードにコピーする
-    document.querySelector("#copyButton").addEventListener("click", () => {
-        document.querySelector("#txt").select();
-        document.execCommand("copy");
+    const searchResult = document.getElementById("resultSelect");
+    const searchinput = document.getElementById("inputSearch");
+
+    //初期表示
+    chrome.storage.local.get(["maxTabNum", "check", "tabGroups", "group", "bArray"], (items) => {
+        document.querySelector("#URLListButton").value = "close";
+        document.querySelector("#tabGroup").checked = items.group;
+        if (items.check == false) {
+            chrome.storage.local.set({ maxTabNum: tabs.length });
+            document.querySelector("#maxTabNum").value = tabs.length;
+        } else {
+            document.querySelector("#maxTabNum").value = items.maxTabNum;
+        };
+        document.querySelector("#check").checked = items.check;
+        document.querySelector("#maxTabNum").min = tabs.length;
+        chrome.storage.local.set({ groupStatus: "domains" });
+        for (let i = 0; i < items.tabGroups.length; i++) {
+            let ul = document.createElement("ul");
+            ul.innerHTML = items.tabGroups[i][0];
+            ul.value = i;
+            let img = document.createElement("img");
+            img.src = "http://www.google.com/s2/favicons?domain=" + items.tabGroups[i][0];
+            ul.prepend(img);
+            document.querySelector("#domains").appendChild(ul);
+        };
+        //rangeの最大タブ数初期表示
+        if (items.check == true) {
+            document.getElementById("nowTabNum").innerHTML = items.maxTabNum;
+            $(".tabRange").css("pointer-events", "auto");
+        } else {
+            document.getElementById("nowTabNum").innerHTML = "∞";
+            $(".tabRange").css("pointer-events", "none");
+        }
+
+        setbackground(items.bArray[2]);
+        //検索結果欄  --shita
+        //--検索結果欄初期表示  あとで検索結果のこしておく処理作りたい
+        const defaultOption = document.createElement("option");
+        defaultOption.text = "検索結果がここに表示されます。";
+        searchResult.replaceChildren(defaultOption);
+        searchResult.disabled = true;
+        //同一タブ削除ボタン --shita
+        switch (items.bArray[1]) {
+            case "0":
+                var onlyTabs = tabs.filter((tab, index, array) => {
+                    return (array.findIndex(nextTab => tab.title === nextTab.title) !== index)
+                });
+                break;
+
+            case "1":
+                var onlyTabs = tabs.filter((tab, index, array) => {
+                    return (array.findIndex(nextTab => tab.url === nextTab.url) !== index)
+                });
+                break;
+
+            case "2":
+                var onlyTabs = tabs.filter((tab, index, array) => {
+                    // if(1 ===(array.findIndex(nextTab => tab.title === nextTab.title) !== index));
+                    return (array.findIndex(nextTab => tab.title === nextTab.title || tab.url === nextTab.url) !== index)
+                });
+                break;
+        };
+        if (onlyTabs.length != 0) {
+            const h_sametab = document.getElementById("sameTabDelete");
+            h_sametab.innerHTML = "重複タブ:" + Number(onlyTabs.length) + "個" + '<button class = "deletetabs_button" id="deleteTabsButton" disabled>削除</button>';
+            const deleteTab = document.getElementById("deleteTabsButton");
+            deleteTab.disabled = false;
+            deleteTab.addEventListener("click", (event) => {
+                chrome.tabs.remove(onlyTabs.map((tab) => Number(tab.id)));
+                h_sametab.textContent = "重複タブ:0個";
+            });
+            $(function () {
+                $(".deletetabs_button").text("削除");
+                $(".deletetabs_button").css({
+                    "box-shadow": "0 5px rgb(35, 100, 35)",
+                    "visibility": "visible"
+                });
+            });
+        };
     });
-    //変更ボタンが押された時実行
-    document.querySelector("#maxTabNumButton").addEventListener("click", () => {
-        chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
-            let maxTabNum = document.querySelector("#maxTabNum").value;
-            //現在のタブ数より小さい値，文字列を設定した場合の処理（キーボードから入力すると設定した最小値より小さい値や文字列が入力できる）
-            if (maxTabNum < tabs.length) {
-                maxTabNum = tabs.length;//現在のタブ数を変数maxTabNumに代入
-                document.querySelector("#maxTabNum").value = tabs.length;//現在のタブ数を表示する．
-            }
-            //ストレージのmaxTabNumに開けるタブ数の最大値を格納
-            chrome.storage.local.set({ maxTabNum: maxTabNum });
+
+    //URLリスト
+    document.querySelector("#URLListButton").addEventListener("click", (event) => {
+        if (event.target.value == "close") {
+            event.target.value = "open";
+            event.target.innerHTML = "閉じる"
+            //textarea
+            let textarea = document.createElement("textarea");
+            let txt = "";
+            tabs.forEach((tab) => {
+                txt += `[${tab.title}] (${tab.url})\n\n`;
+            });
+            textarea.value = txt;
+            document.querySelector("#textarea").appendChild(textarea);
+            //copyButton
+            let button = document.createElement("button");
+            button.innerHTML = "コピー";
+            button.value = "copy";
+            document.querySelector("#copyButton").appendChild(button);
+        }
+        else {
+            event.target.value = "close";
+            event.target.innerHTML = "&ensp;開く&ensp;"
+            document.querySelector("#textarea").replaceChildren();
+            document.querySelector("#copyButton").replaceChildren();
+        };
+    });
+
+    //コピーボタン
+    document.querySelector("#copyButton").addEventListener("click", (event) => {
+        if (event.target.value == "copy") {
+            document.getSelection().selectAllChildren(document.querySelector("#textarea"));
+            document.execCommand('copy');
+        };
+    });
+
+    //最大タブ数
+    document.querySelector("#maxTabNum").addEventListener("change", () => {
+        let maxTabNum = document.querySelector("#maxTabNum").value;
+        if (maxTabNum < tabs.length) {
+            maxTabNum = tabs.length;
+            document.querySelector("#maxTabNum").value = tabs.length;
+        }
+        chrome.storage.local.set({ maxTabNum: maxTabNum });
+        chrome.runtime.sendMessage("changeIcon");
+    });
+
+    //最大タブ数checkbox
+    document.querySelector("#check").addEventListener("change", () => {
+        chrome.storage.local.set({ check: document.querySelector("#check").checked });
+        chrome.runtime.sendMessage("changeIcon");
+    });
+
+    //タブリスト
+    document.querySelector("#domains").addEventListener("click", (event) => {
+        chrome.storage.local.get(["groupStatus", "tabGroups"], (items) => {
+            let result = 0;
+            tabs.forEach((tab) => {
+                if (tab.id == event.target.value) {
+                    chrome.tabs.update((tab.id), { active: true });
+                    result++;
+                };
+            });
+            if (result == 0) {
+                document.querySelector("#domains").replaceChildren();
+                for (let i = 0; i < items.tabGroups.length; i++) {
+                    //domain
+                    let ul = document.createElement("ul");
+                    ul.innerHTML = items.tabGroups[i][0];
+                    ul.value = i;
+                    let img = document.createElement("img");
+                    img.src = "http://www.google.com/s2/favicons?domain=" + items.tabGroups[i][0];
+                    ul.prepend(img);
+                    //title
+                    if (i == event.target.value && items.groupStatus != i) {
+                        for (let j = 2; j < items.tabGroups[i].length; j += 3) {
+                            let li = document.createElement("li");
+                            li.value = items.tabGroups[i][j + 1];//id
+                            li.innerHTML = items.tabGroups[i][j];//title
+                            ul.appendChild(li);
+                            chrome.storage.local.set({ groupStatus: i });
+                        }
+                    }
+                    else if (i == event.target.value) {
+                        chrome.storage.local.set({ groupStatus: "domains" });
+                    }
+                    document.querySelector("#domains").appendChild(ul);
+                };
+            };
         });
     });
+
+    //グループ化checkbox
+    document.querySelector("#tabGroup").addEventListener("change", (event) => {
+        chrome.storage.local.get(["group"], (items) => {
+            if (items.group == false) {
+                chrome.runtime.sendMessage("group");
+            }
+            else {
+                chrome.runtime.sendMessage("ungroup");
+            }
+        });
+    });
+
+    //検索窓 --shita
+    searchinput.addEventListener("change", (event) => {
+        searchResult.disabled = false;
+        searchResult.replaceChildren();
+        chrome.storage.local.get(["bArray"], (value) => {
+            var input = event.target.value.toLowerCase();
+            switch (value.bArray[0]) {
+                case "0":
+                    tabs.forEach((tab) => {
+                        if (tab.title.toLowerCase().indexOf(input) > -1) {
+                            let option = document.createElement("option");
+                            option.value = tab.id;
+                            option.text = tab.title;
+                            searchResult.appendChild(option);
+                        };
+                    });
+                    break;
+                case "1":
+                    tabs.forEach((tab) => {
+                        if (tab.url.toLowerCase().indexOf(input) > -1) {
+                            let option = document.createElement("option");
+                            option.value = tab.id;
+                            option.text = tab.title;
+                            searchResult.appendChild(option);
+                        };
+                    });
+                    break;
+                case "2":
+                    tabs.forEach((tab) => {
+                        if (tab.title.toLowerCase().indexOf(input) > -1 || tab.url.toLowerCase().indexOf(input) > -1) {
+                            let option = document.createElement("option");
+                            option.value = tab.id;
+                            option.text = tab.title;
+                            searchResult.appendChild(option);
+                        };
+                    });
+                    break;
+            }
+            let countTabs = document.createElement("option");
+            countTabs.setAttribute("selected", true);
+            countTabs.text = "ヒット件数: " + searchResult.childElementCount;
+            searchResult.prepend(countTabs);
+        });
+    });
+
+    //検索結果欄  --shita
+    searchResult.addEventListener("change", (event) => {
+        chrome.tabs.update(Number(event.target.value), { active: true });
+    });
+
+    //同一タブ削除ボタン --shita
+    $('#config').on('click', (e) => {
+        if (chrome.runtime.openOptionsPage) {
+            chrome.runtime.openOptionsPage();
+        } else {
+            window.open(chrome.runtime.getURL('options.html'));
+        }
+    });
 });
+
+//メッセージ取得時実行
+chrome.runtime.onMessage.addListener((data) => {
+    chrome.storage.local.get(["group", "maxTabNum"], (items) => {
+        switch (data) {
+            case "changeMaxTabNum":
+                document.querySelector("#maxTabNum").value = items.maxTabNum;
+                break;
+            case "checkON":
+                document.querySelector("#check").checked = true;
+                document.querySelector("#maxTabNum").value = items.maxTabNum;
+                break;
+            case "checkOFF":
+                document.querySelector("#check").checked = false;
+                document.querySelector("#maxTabNum").value = items.maxTabNum;
+                break;
+            default:
+                break;
+        }
+    });
+});
+
+function setbackground(value) {
+    let light = "#f9f9f9";
+    let dark = "#202020";
+    switch (Number(value)) {
+        default:
+        case 0://white
+            $("body").css("background-color", light);
+            break;
+
+        case 1://black
+            $("body").css("background-color", dark);
+            $("body").css("color", light);//とりあえず文字見にくいから設定してるだけ
+            break;
+
+        case 2://auto
+            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                $("body").css("background-color", dark);
+            } else {
+                $("body").css("background-color", light);
+            }
+            break;
+
+        case 3://rgb
+            chrome.storage.local.get(["oArray"], function (value) {
+                $("body").css("background-color", value.oArray[0]);
+            });
+            break;
+
+        case 4://maguro
+            $("body").css("background-image", "url(https://1.bp.blogspot.com/-gq_tAX03Btk/VpjBpezB-kI/AAAAAAAA25Y/s__gB-bb2lc/s1600/bg_natural_ocean.jpg)");
+            $(".main").css("background-image", "url(https://4.bp.blogspot.com/-L-oUiflcmD8/VvXe7bOhc3I/AAAAAAAA5KE/YlzixMhJl-UdBETW5PstRAAfqqNyH84QQ/w1200-h630-p-k-no-nu/fish_maguro2.png)");
+            break;
+
+        case 5://img
+            chrome.storage.local.get(["oArray", "rArray"], function (value) {
+                if (Boolean(value.oArray[1])) {
+                    $("body").css("background-image", "url(" + value.oArray[1] + ")");
+                    $("body").css("background-repeat", "no-repeat");
+                    $("body").css("background-size", "cover");
+                    // $("body").css("position","relative");
+                    $("body::before").css("background-color", "rgba(0, 0, 0, " + value.rArray[0] / 100 + ")");
+                } else {
+                    $("body").css("background-image", "url(https://1.bp.blogspot.com/-gq_tAX03Btk/VpjBpezB-kI/AAAAAAAA25Y/s__gB-bb2lc/s1600/bg_natural_ocean.jpg)");
+                    $(".main").css("background-image", "url(https://4.bp.blogspot.com/-L-oUiflcmD8/VvXe7bOhc3I/AAAAAAAA5KE/YlzixMhJl-UdBETW5PstRAAfqqNyH84QQ/w1200-h630-p-k-no-nu/fish_maguro2.png)");
+                };
+
+            });
+            break;
+    };
+};
